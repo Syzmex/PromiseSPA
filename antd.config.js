4b992c657f637f4fdcdd24d32b32f493d26c1c55
mArker <252133226@qq.com>
2017-02-10 14:02:24 +0800

    base framework

---
+++ antd.config.js
@@ -0,0 +1,7 @@
+// available paremters: https://github.com/ant-design/ant-design/blob/master/components/style/themes/default.less
+
+const path = require( 'path' );
+
+module.exports = {
+  // '@icon-url': `"${path.relative( './~/antd/lib/style/*', './src/assets/fonts/iconfont' )}"`
+};
---
+++ src/routes/IndexPage.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './IndexPage.less';
+
+function IndexPage() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+IndexPage.propTypes = {};
+
+export default IndexPage;
