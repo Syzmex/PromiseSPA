8aa9cfdb9db9f52bf3a0cc0639d17ca2936afbde
mArker <252133226@qq.com>
2017-02-24 15:38:10 +0800

    add test

--- test/example-test.js
+++ test/example-test.js
@@ -7,16 +7,29 @@
 // import { add } from '../src/utils/add';
 
 var expect = require('expect');
-var add = require('../src/utils/add').add;
+var utils = require('../src/utils/add');
+var add = utils.add;
+var multiply = utils.multiply;
 
 // 函数测试
 describe( '#utils', function () {
+
   describe( '#add()', function () {
     it( '加法1', function () {
       expect( add( 1, 2 ) ).toEqual( 3 );
     } );
-    it( '加法2', () => {
+    it( '加法2', function () {
       expect( add( 6, 7 ) ).toEqual( 13 );
     } );
   } );
+
+  describe( '#multiply()', function () {
+    it( '乘法1', function () {
+      expect( multiply( 1, 2 ) ).toEqual( 2 );
+    } );
+    it( '乘法2', function () {
+      expect( multiply( 6, 7 ) ).toEqual( 42 );
+    } );
+  } );
+
 } );
